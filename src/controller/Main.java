cf7c8a196829be6b76eceef750b3e3cfcdf59360
harshadvict <harshadvict@gmail.com>
2020-09-24 20:39:03 +0530

    harsh

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,28 @@
+package controller;
 
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+
+import dao.SkillDAO;
+import model.Skill;
+
+class Main{
+	public static void main(String[] Args) throws ClassNotFoundException, SQLException {
+		System.out.println("List of all Skills");
+		
+		SkillDAO skillobj=new SkillDAO();
+		//storing value in the variable of type list
+		List<Skill> list=skillobj.listAllSkills();
+		//to iterate over the list
+		Iterator<Skill> iterator=list.iterator();
+		int i=1;
+		while(iterator.hasNext()) {
+			
+			Skill skillobj1=iterator.next();
+			System.out.println(i+"."+skillobj1.getSkillName());
+			i++;
+		}
+		
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,43 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO{
+	public List<Skill> listAllSkills () throws ClassNotFoundException, SQLException{
+		
+		
+		//function of this method
+		//The above method is used to fetch all the skills from skill table and returns the list of skills.
+		//Display the skills in ascending order based on name.
+		
+		ConnectionManager con =new ConnectionManager();
+		Connection conn=con.getConnection();
+		String sql="select * from skill";
+		PreparedStatement stmt=conn.prepareStatement(sql);
+		ResultSet rs=stmt.executeQuery();
+		List<Skill> skilllist=new ArrayList<>();
+		while(rs.next()) {
+			//printing value of the first column for testing
+			//System.out.println(rs.getString(1));
+			
+			//loading value of first and second column in the variable
+			Long id=Long.parseLong(rs.getString(1));
+			String skill=rs.getString(2);
+			
+			//calling constructor of skill class
+			Skill skillobj=new Skill(id,skill);
+			//System.out.println("flow is correct");
+			skilllist.add(skillobj);
+		}
+		return skilllist;
+		
+	}
+}
